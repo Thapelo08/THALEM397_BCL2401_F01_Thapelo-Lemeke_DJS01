55f20de9979c44e249177eac6ae2ec2d5558ad4f
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-05-07 15:47:43 +0200

    Added `<meta>` tag

--- StudentNo_Classcode_Group_Name-Surname_DJS01-main/index.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS01-main/index.js
@@ -14,13 +14,13 @@ const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
+const d2 = d + (vel*time); //calcultes new distance
 const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+const vel2 = calcNewVel(vel, acc, time); //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
-calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+ calcNewVel = (vel, acc, time) => { 
+  return vel + (acc*time);
 }
 
 console.log(`Corrected New Velocity: ${vel2} km/h`);
